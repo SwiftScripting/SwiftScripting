9f98e98f1dbc0a4bc0e6c72f39d7fb8d9eb8e3d6
Paul Calnan <me@paulcalnan.com>
2017-02-22 22:59:49 -0500

    Script to add generated repo to Github

---
+++ bin/add-to-github.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# set -x
+set -euo pipefail
+E_BADARGS=85
+
+if [ "$#" -ne 1 ]
+then
+    echo "Usage $(basename $0) path-to-package"
+    exit $E_BADARGS
+fi
+
+path_to_package="$1"
+cd "$path_to_package"
+
+name="$(basename $(pwd))"
+hub create "SwiftScripting/$name"
+git push origin master
